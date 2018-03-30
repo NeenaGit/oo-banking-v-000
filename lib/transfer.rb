788a2c5848e021f9a9b51f47a97272c21e23d15b
Neena van de Water <nkuriakose@hotmail.com>
2018-03-30 19:47:42 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,8 +22,9 @@ class Transfer
 
   def execute_transaction
    if valid? && sender.balance > amount && self.status
-     sender.balance -= amount 
+     sender.balance -= amount
      reciever.balance += amount
+     @status = "complete" 
   end
 
   def reverse_transfer
