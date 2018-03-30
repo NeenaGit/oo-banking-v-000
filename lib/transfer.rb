f6330af79b15f64d0279739a5df71c89a2718b23
Neena van de Water <nkuriakose@hotmail.com>
2018-03-30 19:31:21 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,31 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount
+  attr_reader :status
+
+  def initialize(sender, receiver, amount)
+    @amount = amount
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+  end
+
+  def valid?
+    if self.sender.valid? && self.receiver.valid?
+      true
+    else
+      false
+    end
+  end
+
+  # def transfer
+  # end
+
+  def execute_transaction(money)
+    BankAccount.balance.transfer << money
+  end
+
+  def reverse_transfer
+
+  end
+
 end
